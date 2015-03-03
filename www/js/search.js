881afd7404d7c9206ffb07991480243ec1ce9081
mpmfrans <mpmfrans@gmail.com>
2015-03-03 21:44:04 +0100

    Search.js fix http get

--- www/js/search.js
+++ www/js/search.js
@@ -1,7 +1,7 @@
 $("#search").on('click', function(){
     $.ajax({
         type: 'GET'
-        url: 'https://api.eet.nu/venues/1'
+        url: 'https://api.eet.nu/venues?ids=1'
         succes: function(restaurants){
             $.each(restaurants, function(i, restaurant) {
                 $("#restaurants").append('<li>my resaurants</li>'); 
