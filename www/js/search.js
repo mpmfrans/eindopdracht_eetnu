2e0908f172a570d28370cd5a9aa5003af85fb8c0
mpmfrans <mpmfrans@gmail.com>
2015-03-03 20:39:53 +0100

    restaurants list

---
+++ www/js/search.js
@@ -0,0 +1,17 @@
+$(function (){
+    
+    var $restaurants = $('#restaurants');
+    
+    $.ajax({
+        type: 'GET'
+        url: 'https://api.eet.nu/venues'
+        succes: function(restaurants){
+            $.each(restaurants, function(i, restaurant) {
+                $restaurants.append('<li>my resaurants</li>'); 
+            });
+            
+        }
+    });
+    
+});
+
