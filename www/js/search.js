6b50af5609f79565598437334b8d226ff13ff5b6
mpmfrans <mpmfrans@gmail.com>
2015-03-03 21:47:39 +0100

    search.js fix successsss

--- www/js/search.js
+++ www/js/search.js
@@ -2,7 +2,7 @@ $("#search").on('click', function(){
     $.ajax({
         type: 'GET'
         url: 'https://api.eet.nu/venues?ids=1'
-        succes: function(restaurants){
+        success: function(restaurants){
             $.each(restaurants, function(i, restaurant) {
                 $("#restaurants").append('<li>my resaurants</li>'); 
             }); 
