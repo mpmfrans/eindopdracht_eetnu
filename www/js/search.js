d3ce1d612279ab778d854756e1c53c5c3f7f7d1b
mpmfrans <mpmfrans@gmail.com>
2015-03-03 20:59:45 +0100

    other try

--- www/js/search.js
+++ www/js/search.js
@@ -6,7 +6,7 @@ search: function(){
     
     $.ajax({
         type: 'GET'
-        url: 'https://api.eet.nu/venues'
+        url: 'https://api.eet.nu/venues/1'
         succes: function(restaurants){
             $.each(restaurants, function(i, restaurant) {
                 $restaurants.append('<li>my resaurants</li>'); 
