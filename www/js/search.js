91576f96718a69972eba62282d2220e4cd87a431
mpmfrans <mpmfrans@gmail.com>
2015-03-03 21:52:42 +0100

    Search test

--- www/js/search.js
+++ www/js/search.js
@@ -1,12 +1,5 @@
 $("#search").on('click', function(){
-    $.ajax({
-        type: 'GET'
-        url: 'https://api.eet.nu/venues?ids=1'
-        success: function(restaurants){
-            $.each(restaurants, function(i, restaurant) {
-                $("#restaurants").append('<li>my resaurants</li>'); 
-            }); 
-        }
-    });
+   alert('hi');
+   $.("#reasturants").append('<li>Test pagina</li>')
 });
 
