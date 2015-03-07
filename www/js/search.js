2a33bb2acf6e7c9c3a14d4e53d9512e8fde89bd2
mpmfrans <mpmfrans@gmail.com>
2015-03-07 13:37:27 +0100

    fix search restaurants

--- www/js/search.js
+++ www/js/search.js
@@ -16,36 +16,35 @@ var searchRestaurants = {
             
            
             
-            console.log(index);
+            //console.log(str_index);
             var restaurants = $("#restaurants");
         
             if(index == null || index == "null"){
-                var str_link = "https://api.eet.nu/venues?geolocation=" + Geo.lat + "," + Geo.lng + "&max_distance="+ search_range +"&page=1&per_page=30";
+                var count = 1;
             }else{
-                 var str_link = decodeURIComponent(index);
-                 
+                 count =  index;
             }
 //            
-            //console.log(count);
+            console.log(count);
             //localStorage.setItem("counter", counter);
              
                 
                 $.ajax({
                 type: 'GET',
-                //url: "https://api.eet.nu/venues?geolocation=" + Geo.lat + "," + Geo.lng + "&max_distance="+ search_range +"&page=1&per_page=30",
-                url:  str_link,
+                url: "https://api.eet.nu/venues?geolocation=" + Geo.lat + "," + Geo.lng + "&max_distance="+ search_range +"&page="+count+"&per_page=30",
+               
                 success: function(data){
                     
                     restaurants.empty();
                     restaurants.append("<h3>Search results:</h3>");
                     var total_pages = data.pagination.total_pages;
-                    var next_page = '"'+ data.pagination.next_page + '"';
-                    var current_page = data.pagination.current_page;
+                    //var next_page = '"'+ data.pagination.next_page + '"';
+                   // var current_page = data.pagination.current_page;
                    
                     
                     console.log(total_pages);
-console.log(current_page);
-                    console.log(next_page);
+//console.log(current_page);
+                    //console.log(next_page);
                     
                     $.each(data.results, function(i, restaurant){
                         var name = restaurant.name;
@@ -63,31 +62,31 @@ console.log(current_page);
                   
                       // restaurants.append("<br><button id='next' onclick=searchRestaurants.getCurrentLocation(" + count + ");>next</button>"); 
                    
-                    if(current_page == 1 && current_page < total_pages){
+                    if(count == 1 && count < total_pages){
                         restaurants.append("<br><div><button id='next' class='ui-btn' data-inline='true'>next</button></div>");
                         $("#next").click( function(){
-                            //count++;                 
-                            searchRestaurants.getCurrentLocation(next_page);                 
+                            count++;                 
+                            searchRestaurants.getCurrentLocation(count);                 
                                          
                         });
                     
-                    }else if(current_page >= 2 && current_page < total_pages){
+                    }else if(count >= 2 && count < total_pages){
                         restaurants.append("<br><div><button id='next' class='ui-btn'>next</button><button id='previous' class='ui-btn'>previous</button></div>");
                         $("#next").click( function(){
-                            //count++;                 
-                            searchRestaurants.getCurrentLocation(next_page);                 
+                            count++;                 
+                            searchRestaurants.getCurrentLocation(count);                 
                                          
                         });
                         $("#previous").click( function(){
-                            //count--;                 
-                            searchRestaurants.getCurrentLocation(data.pagination.previous_page);                 
+                            count--;                 
+                            searchRestaurants.getCurrentLocation(count);                 
                                          
                         });
-                    }else if(current_page == total_pages && next_page == "null"){
+                    }else if(count == total_pages){
                         restaurants.append("<br><div><button id='previous'>previous</button></div>");
                         $("#previous").click( function(){
-                           // count--;                 
-                            searchRestaurants.getCurrentLocation(data.pagination.previous_page);                 
+                            count--;                 
+                            searchRestaurants.getCurrentLocation(count);                 
                                          
                         });
                     
