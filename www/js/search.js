11cf1788bfe8d1e3d2a9aea642fb0fb1940b177a
mpmfrans <mpmfrans@gmail.com>
2015-03-09 22:32:53 +0100

    Details_page and website url and phonecall

--- www/js/search.js
+++ www/js/search.js
@@ -1,3 +1,6 @@
+
+
+
 var searchRestaurants = {
     
     getCurrentLocation: function(index) {
@@ -24,8 +27,6 @@ var searchRestaurants = {
             }else{
                  count =  index;
             }
-            
-            console.log(count);
                 
                 $.ajax({
                 type: 'GET',
@@ -41,18 +42,20 @@ var searchRestaurants = {
                         var name = restaurant.name;
                         var category = restaurant.category;
                         var telephone = restaurant.telephone;
+                        var id = restaurant.id;
                         /*
-                       Following code can be used to create collapsible items    
-                        
-                       restaurants.append($("<div data-role='collapsible' data-collapsed='true' data-mini='true'><h3>"+name+"Category: "+ category+
-                                            "</h3><span> Telephone: " + telephone +"</span></div>"));
-                       restaurants.find('div[data-role=collapsible]').collapsible();    
+                           Following code can be used to create collapsible items    
+
+                           restaurants.append($("<div data-role='collapsible' data-collapsed='true' data-mini='true'><h3>"+name+"Category: "+ category+
+                                                "</h3><span> Telephone: " + telephone +"</span></div>"));
+                           restaurants.find('div[data-role=collapsible]').collapsible();    
                         */
+                       restaurant_list.append($("<li><a href='#details_page' id='restaurant_details' data-icon='arrow-r' data-role='listview' data-id="+id+">"+name+"</a></li>"));
+                       restaurant_list.listview('refresh');  
                         
-                       restaurant_list.append($("<li><a href='#' data-icon='arrow-r' data-role='listview'>"+name+"</a></li>"));
-                       restaurant_list.listview('refresh');
-                            
                     });
+                    
+                  
                    
                     if(count == 1 && count < total_pages){
                         
@@ -63,8 +66,7 @@ var searchRestaurants = {
                         
                         $("#next").on('tap', function(){
                             count++; 
-                            searchRestaurants.getCurrentLocation(count);                 
-                                         
+                            searchRestaurants.getCurrentLocation(count);                                 
                         });
                     
                     }else if(count >= 2 && count < total_pages){
@@ -76,14 +78,14 @@ var searchRestaurants = {
                         
                         $("#next").on('tap', function(){
                             count++;   
-                            searchRestaurants.getCurrentLocation(count);                 
-                                         
+                            searchRestaurants.getCurrentLocation(count);                                
                         });
                         $("#previous").on('tap', function(){
                             count--;  
                             searchRestaurants.getCurrentLocation(count);                 
                                          
                         });
+                        
                     }else if(count == total_pages && count != 1){
                         restaurant_list.append("<p>["+count+"-"+total_pages+"]</p>");
                         restaurant_list.append("<fieldset class='ui-grid-a'>");
@@ -91,14 +93,13 @@ var searchRestaurants = {
                         restaurant_list.append("</fieldset>");
                         $("#previous").on('tap', function(){
                             count--;    
-                            searchRestaurants.getCurrentLocation(count);                 
-                                         
+                            searchRestaurants.getCurrentLocation(count);                               
                         });
                     }
                    
-                    }
+                }
                 });
-            } 
+        } 
      
         function error(){
             alert("Get current geolocation failed");
@@ -107,9 +108,34 @@ var searchRestaurants = {
     }
 };
 
- 
+$(document).on('tap', '#restaurants li a', function(e){
+    var RestaurantId = ($(this).data("id"));
+    localStorage.setItem("RestaurantDetail", RestaurantId);
+});
 
 
+$(document).on("pagebeforeshow","#details_page",function(event){
+    
+     $("#details_content").empty();
+    
+     $.ajax({
+        type: 'GET',
+        url: "https://api.eet.nu/venues/" + localStorage.getItem("RestaurantDetail"),
+        
+        success: function(restaurant){
+            var name = restaurant.name;
+            var telephone = restaurant.telephone;
+            var website_url = restaurant.website_url;
+            
+            $("#details_content").append("<h1>"+name+"</h1>");
+            $("#details_content").append('<a href="tel:'+telephone+'">'+telephone+'</a></br></br>');
+            $("#details_content").append("<a href="+website_url+">"+website_url+"</a>");
+            
+        }        
+    
+    });
+   
+});
 
 
 
