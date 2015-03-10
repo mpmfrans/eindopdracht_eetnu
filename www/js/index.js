8a76d0a3c29b42592dac004653443fb00dfe991f
mpmfrans <mpmfrans@gmail.com>
2015-03-10 20:43:19 +0100

    again

--- www/js/index.js
+++ www/js/index.js
@@ -53,14 +53,13 @@ $('div.ui-page').on('swipeleft swiperight', function(event){
      if (event.type == "swiperight"){
         var nextpage = $(this).next('div[data-role="page"]');
         if (nextpage.length > 0) {
-            $.mobile.changePage(nextpage, "slide", false, true);
+            $.mobile.changePage(nextpage, {transition: "slide"});
         }
      }
     if (event.type == "swipeleft"){
         var prevpage = $(this).prev('div[data-role="page"]');
         if (prevpage.length > 0) {
-        $.mobile.changePage(prevpage, {transition: "slide",
-        reverse: true}, true, true);
+        $.mobile.changePage(prevpage, {transition: "slide"});
         }
     }
 });
