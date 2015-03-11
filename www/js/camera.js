ef4db6d317e7912c9d37b9a66f08ccd58a2f1a0f
mpmfrans <mpmfrans@gmail.com>
2015-03-11 22:02:30 +0100

    another camera try

--- www/js/camera.js
+++ www/js/camera.js
@@ -1,13 +1,14 @@
-function takePicture(){      
-        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+$(document).on('tap','#take_a_picture',function(e){
+        
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
         destinationType: Camera.DestinationType.FILE_URI });
 
         function onSuccess(imageURI) {
-            var image = document.getElementById('myImage');
-            image.src = imageURI;
-        }
-
-        function onFail(message) {
-            alert('Failed because: ' + message);
-        }
+        var image = document.getElementById('myImage');
+        image.src = imageURI;
     }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+});
