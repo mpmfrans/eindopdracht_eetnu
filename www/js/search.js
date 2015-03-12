b08f3b9f77b2c11b0d3b532c3be4e3fea05eb05b
mpmfrans <mpmfrans@gmail.com>
2015-03-12 13:52:46 +0100

    without stars

--- www/js/search.js
+++ www/js/search.js
@@ -149,20 +149,19 @@ $(document).on("pagebeforeshow","#details_page",function(event){
             $("#details_content").append("<p>Website: <a href="+website_url+">"+website_url+"</a></p>"); 
             $("#details_content").append("<div id='jRate'></div><div id='rate_number'></div>"); 
             $("#details_content").append("<div id='htmlRating' data-role='igrating' data-value='2'></div>"); 
+            $("#details_content").append("<div id='htmlRating' data-role='igrating' data-value='2'>"); 
             
-                
             
-            
-            $("#jRate").jRate({
-                    startColor: 'orange',
-		            endColor: 'orange',
-                    width: 30,
-		            height: 30,
-                    min: 0,
-		            max: 10,
-                    rating: rating_res,
-                    readOnly: true
-            });
+//            $("#jRate").jRate({
+//                    startColor: 'orange',
+//		            endColor: 'orange',
+//                    width: 30,
+//		            height: 30,
+//                    min: 0,
+//		            max: 10,
+//                    rating: rating_res,
+//                    readOnly: true
+//            });
             $('#rate_number').text("Rating: "+rating_res);
             console.log(rating_res);
             
