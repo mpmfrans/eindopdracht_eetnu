29accf8bccac5bc9832b5660c1e305b861b0641c
mpmfrans <mpmfrans@gmail.com>
2015-03-12 16:29:34 +0100

    add cordova script

---
+++ www/js/camera.js
@@ -0,0 +1,16 @@
+        $(document).on('tap','#take_a_picture',function(e){
+        
+     if (navigator.camera) {
+            navigator.camera.getPicture(onSuccess, onFail, { quality: 50 });
+     }
+    
+    function onSuccess(imageURI) {
+        var image = document.getElementById('myImage');
+        image.src = imageURI;
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+});
+       
